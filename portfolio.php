62733b07c05e48fac753a5b8da8ba1856a771e9b
andi <andi@narancs.net>
2014-08-23 20:03:28 +0200

    fit-talent

--- portfolio.php
+++ portfolio.php
@@ -130,7 +130,7 @@ function buildPortfolioContent($portfolioData, $columnCount = 4) {
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
